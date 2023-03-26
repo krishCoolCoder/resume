4c849ae91d621b5ce830d505e45058fe0d5698b2
saikrishnakingtyle <77505364+saikrishnakingtyle@users.noreply.github.com>
2023-03-26 18:20:13 +0530

    added a 3D trial

---
+++ main.js
@@ -0,0 +1,40 @@
+const html = document.documentElement;
+const canvas = document.querySelector(".magic");
+const context = canvas.getContext('2d');
+
+const currentFrame = index => (
+    `../boxImages/${index.toString().padStart(6,'0')}.png`
+)
+
+const frameCount = 77;
+
+canvas.height = 1500;
+canvas.width = 1158;
+const img = new Image();
+img.src = currentFrame(1);
+img.onload = function() {
+    // context.drawImage(img,0,0);
+    context.drawImage(img,(canvas.width-img.width)/2, (canvas.height-img.height)/2);
+}
+
+const updateImage = index => {
+    img.src = currentFrame(index);
+    // context.drawImage(img, 0, 0);
+    context.drawImage(img, (canvas.width-img.width)/2 ,(canvas.height-img.height)/2);
+}
+
+window.addEventListener('scroll', () => {
+    const scrollTop = html.scrollTop;
+    const maxScrollTop = html.scrollHeight - window.innerHeight;
+    const scrollFraction = scrollTop / maxScrollTop;
+    const frameIndex = Math.min(frameCount -  1, Math.floor(scrollFraction * frameCount));
+    requestAnimationFrame(()=> updateImage(frameIndex < 0 ? 1 : frameIndex + 1));
+})
+
+// const preloadImages = ()=> {
+//     for (let i = 1; i < frameCount; i++) {
+//         const img = new Image();
+//         img.src = currentFrame(i);
+//     }
+// }
+// preloadImages();
